54ef9436f213523319047a68dd520e95430b5fb5
ongjoshua <joshuaspencer.ong.iics@ust.edu.ph>
2021-05-17 21:05:15 +0800

    Deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true, 
+  firebase :{
+    apiKey: "AIzaSyBTRcYBzEsw1Hl1bDrtc5mB-cC17PazJlw",
+    authDomain: "blazeathletics-e3a6e.firebaseapp.com",
+    databaseURL: "https://blazeathletics-e3a6e-default-rtdb.firebaseio.com",
+    projectId: "blazeathletics-e3a6e",
+    storageBucket: "blazeathletics-e3a6e.appspot.com",
+    messagingSenderId: "26474255563",
+    appId: "1:26474255563:web:77f95f1baf463c8758f74e",
+    measurementId: "G-H1V5EJG8Z1"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false, 
+  production: true, 
   firebase :{
     apiKey: "AIzaSyBTRcYBzEsw1Hl1bDrtc5mB-cC17PazJlw",
     authDomain: "blazeathletics-e3a6e.firebaseapp.com",
